a7f95184d2a084581c061c862c28fce8afdfdf45
aptx113 <aptx113@gmail.com>
2021-12-06 18:18:02 +0800

    Construct function to parse Json from Assets

---
+++ app/src/main/java/com/danteyu/studio/crypto/data/source/api/DefaultJsonService.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Dante Yu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.danteyu.studio.crypto.data.source.api
+
+import android.content.Context
+import com.danteyu.studio.crypto.ext.generateObjectsFromAsset
+import com.danteyu.studio.crypto.model.CurrencyInfo
+import com.squareup.moshi.Moshi
+
+/**
+ * Created by George Yu in Dec. 2021.
+ */
+class DefaultJsonService(
+    private val context: Context,
+    private val moshi: Moshi,
+    private val fileName: String
+) :
+    JsonService {
+
+    override fun getCurrencyInfoFromAsset(): List<CurrencyInfo>? {
+        return context.generateObjectsFromAsset(moshi, fileName)
+    }
+}
